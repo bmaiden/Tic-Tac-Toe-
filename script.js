d7b2f927872d1bf6706f018e73944169868df5a9
bmaiden <b.maiden2012@yahoo.com>
2023-11-18 12:49:35 -0600

    display empty tic-tac-toe board on initially display

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// 1) Display an empty tic-tac-toe board when the page is initially displayed.
+// 1) Display an empty tic-tac-toe board when the page is initially displayed.  √
 // 2) A player can click on the nine cells to make a move.
 // 3) Every click will alternate between marking an X and O.
 // 4) Once occupied with an X or O, the cell cannot be played again.
