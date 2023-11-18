dd747e78279126fe95ddc99d6b36de83cc121d6d
bmaiden <b.maiden2012@yahoo.com>
2023-11-18 11:56:20 -0600

    requirements added to JS script

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("This is working!")
+
+// 1) Display an empty tic-tac-toe board when the page is initially displayed.
+// 2) A player can click on the nine cells to make a move.
+// 3) Every click will alternate between marking an X and O.
+// 4) Once occupied with an X or O, the cell cannot be played again.
+// 5) Provide a Reset Game button that will clear the contents of the board.
+
+//Testing to make sure HTML & CSS are linked to JS  √
+//console.log("This is working!")
+
+/*----- constants -----*/
+
+
+
+
+/*----- state variables -----*/
+
+
+
+
+/*----- cached elements  -----*/
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+/*----- functions -----*/
+
+
